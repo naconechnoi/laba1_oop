92d1bf9b581c03a2f4d96dae901523252d821da5
anastasiia <anastasiia_sadova@knu.ua>
2021-10-12 17:09:45 +0300

    build

---
+++ cmake-build-debug/CMakeFiles/untitled28.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled28.dir/main.cpp.o"
+  "untitled28"
+  "untitled28.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled28.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
